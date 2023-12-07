acffa68ecf10f99a26b3c70e84f0ab555d475b9b
Domingos Dias <domingos.adj@gmail.com>
2023-12-07 18:48:19 -0300

    taxonomic_distinction

--- phylogenetic_indices/indices.py
+++ phylogenetic_indices/indices.py
@@ -28,3 +28,38 @@ def species_richness(features: np) -> int:
         return num_species
     else:
         raise ValueError('Unsupported data type')
+
+
+def taxonomic_distinction(hist: np) -> int:
+    """
+    Calcula o índice de distinção taxonômica.
+
+    Args:
+      hist: Um histograma de abundância de espécies.
+
+    Returns:
+      O índice de distinção taxonômica.
+
+    Examples:
+    >>> import numpy as np
+    >>> hist = np.array([10, 20, 30])
+    >>> taxonomic_distinction(hist)
+    0.7909604519774012
+    """
+
+    # num_species = len(hist)
+    num_species = species_richness(hist)
+
+    mean_dist = 0
+    for i in range(num_species):
+        for j in range(i + 1, num_species):
+            dist = abs(i - j)
+            mean_dist += dist * hist[i] * hist[j]
+
+    total_abundance = np.sum(hist)
+
+    taxonomic_distinction = (
+        2 * mean_dist / (total_abundance * (total_abundance - 1))
+    )
+
+    return taxonomic_distinction
